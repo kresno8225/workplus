9c4787f199d46e67f295288499a84b87394d6439
KRESNO <38207787+kresno8225@users.noreply.github.com>
2024-08-01 14:10:56 +0700

    first upload
    First upload

---
+++ answer1/answer1/answer1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace answer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testcase();
+        }
+
+        public static void testcase()
+        {
+            int[] ins = { 2, 3, 4, 5 };
+            foreach (var item in weightedstring("gigi", ins))
+            {
+                Console.WriteLine(item.ToString());
+            }
+            var i = Console.ReadLine();
+        }
+
+        public static string[] weightedstring(string inputs, int[] nums)
+        {
+            int i = 0;
+            int indeks = 0;
+            List<int> cur_i = new List<int>();
+            char prev_c = '\0';
+            string[] ret = nums.Select(x => x.ToString()).ToArray();
+
+            foreach (char c in inputs)
+            {
+
+                if ((prev_c != '\0') && (c == prev_c))
+                {
+                    i = i + (char.ToUpper(c) - 64);
+                    cur_i[cur_i.Last()] = i;
+                }
+                else
+                {
+                    i = char.ToUpper(c) - 64;
+                    cur_i.Add(i);
+                }
+
+                prev_c = c;
+            }
+
+            i = 0;
+
+            foreach (int x in cur_i)
+            {
+                if (x == nums[i])
+                {
+                    ret[i] = "yes";
+                }
+                else
+                {
+                    ret[i] = "no";
+                }
+                i++;
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ answer3/answer3/answer3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace answer3
+{
+    class Program
+    {
+        static String pilink(String str,
+                                          int k)
+        {
+            char[] palin = str.ToCharArray();
+            String ans = "";
+
+            int l = 0;
+            int r = str.Length - 1;
+
+            while (l < r)
+            {
+
+                if (str[l] != str[r])
+                {
+                    palin[l] = palin[r]
+                      = (char)Math.Max(str[l], str[r]);
+                    k--;
+                }
+                l++;
+                r--;
+            }
+            if (k < 0)
+            {
+                return "Errs";
+            }
+
+            l = 0;
+            r = str.Length - 1;
+
+            while (l <= r)
+            {
+                if (l == r)
+                {
+                    if (k > 0)
+                    {
+                        palin[l] = '9';
+                    }
+                }
+                if (palin[l] < '9')
+                {
+
+                    if (k >= 2 && palin[l] == str[l]
+                        && palin[r] == str[r])
+                    {
+                        k -= 2;
+                        palin[l] = palin[r] = '9';
+                    }
+
+                    else if (k >= 1
+                             && (palin[l] != str[l]
+                                 || palin[r] != str[r]))
+                    {
+                        k--;
+                        palin[l] = palin[r] = '9';
+                    }
+                }
+                l++;
+                r--;
+            }
+            for (int i = 0; i < palin.Length; i++)
+                ans += palin[i];
+            return ans;
+        }
+
+        public static void Main()
+        {
+            String str = "73533";
+            int k = 3;
+            Console.Write(pilink(str, k));
+            Console.ReadLine();
+        }
+    }
+}
